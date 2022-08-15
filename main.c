bc0ac3b257da70361e8c8a09a84b0f6af17604a3
Gabriel Scandiuzzi <93269738+gabrielscand@users.noreply.github.com>
2022-08-15 16:48:38 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int idade;
+   printf("Informe sua idade: ");
+   scanf("%i", &idade);
+   if(idade>=5 && idade<=7)
+    printf("Infantil A");
+   else if (idade>=8 && idade<=10)
+   printf("Infantil B");
+   else if (idade>=11 && idade<=13)
+   printf("Juvenil A");
+   else if (idade>=14 && idade<=17)
+    printf("Juvenil B");
+   else if (idade<=4)
+    printf("Inadequado");
+   else
+   printf("Adulto");
+
+    return 0;
+}
